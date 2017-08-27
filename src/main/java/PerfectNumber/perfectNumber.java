883b76abefd81f32ab5ae57b3a1659c18df08e2f
Artur <arturbra@gmail.com>
2017-08-27 15:48:49 +0200

    + Perfect Number

---
+++ src/main/java/PerfectNumber/perfectNumber.java
@@ -0,0 +1,27 @@
+package PerfectNumber;
+
+import java.util.Scanner;
+
+/**
+ * Created by artur on 27.08.2017.
+ */
+public class perfectNumber {
+    public static void main(String[] args) {
+        System.out.print("Podaj liczbę: ");
+        Scanner scanner = new Scanner(System.in);
+        int candidateToPerfectNumber = scanner.nextInt();
+        int sumOfModulo = 0;
+        for (int i = 1; i <= candidateToPerfectNumber / 2; i++) {
+            if (candidateToPerfectNumber % i == 0) {
+                sumOfModulo += i;
+            }
+        }
+
+        if (candidateToPerfectNumber == sumOfModulo) {
+            System.out.print("Liczba jest doskonała ");
+        } else {
+            System.out.print("Liczba nie jest doskonała ");
+        }
+
+    }
+}
